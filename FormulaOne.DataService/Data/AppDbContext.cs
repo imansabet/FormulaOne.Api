57f3561c3cc0724795c2864936d09f2e0a45d4ba
iman <dev.imansabet@gmail.com>
2024-12-03 21:51:23 +0330

    Add Db Entities - Add Connection strings & config Database

---
+++ FormulaOne.DataService/Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using FormulaOne.Entities.DbSet;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System.Security.Cryptography.X509Certificates;
+
+namespace FormulaOne.DataService.Data;
+
+public class AppDbContext : DbContext
+{
+    // Define Db Entities
+    public virtual DbSet<Driver> Drivers { get; set; }
+    public virtual DbSet<Achievement> Achievements { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+    {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        // Specified RelationShip between entities
+        modelBuilder.Entity<Achievement>(entity =>
+        {
+            entity.HasOne(d => d.Driver)
+            .WithMany(p => p.Achievements)
+            .HasForeignKey(d => d.DriverId)
+            .OnDelete(DeleteBehavior.NoAction)
+            .HasConstraintName("FK_Achievements_Driver");
+        });
+    }
+
+
+
+
+
+}
